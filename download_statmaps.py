604e88ba0a3287396be1c04654a823944d3702e0
erramuzpe <asier.erramuzpe@gmail.com>
2016-05-19 12:09:47 +0200

    added download script from vsoch/brainart, I will still wait to download the whole set

--- download_statmaps.py
+++ download_statmaps.py
@@ -1,2 +1,23 @@
-# if necessary, download statmaps for data testing with this script
+# if necessary, download statmaps for data testing with this script. Credit: vsoch/brainart
+from pyneurovault import api
+import numpy as np
 
+all_collections = api.get_collections()
+collections_with_dois = all_collections[np.logical_not(all_collections.DOI.isnull())]
+
+images = api.get_images(collection_pks=list(collections_with_dois.collection_id))
+
+# Remove images that are thresholded
+images = api.filter(df=images,column_name="is_thresholded",field_value=False)
+
+# Not in MNI
+images = api.filter(df=images,column_name="not_mni",field_value=False)
+
+# Just fMRI bold
+images = api.filter(df=images,column_name="modality",field_value="fMRI-BOLD")
+
+# Download all images to file, resample to target
+outfolder = "/home/asier/git/neurovault_benchmark/ann-benchmark/data"
+
+# If you don't want to resample
+api.download_images(dest_dir = outfolder,images_df=images,resample=False)
