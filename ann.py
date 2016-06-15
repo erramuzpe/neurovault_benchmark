64e38742c21965ac6f1d88952b4f664f293f4d07
erramuzpe <asier.erramuzpe@gmail.com>
2016-06-15 15:11:21 +0200

    Try to configure REDIS with Nearpy

--- ann.py
+++ ann.py
@@ -123,9 +123,38 @@ for i in range(features.shape[0]):
         results = nearpy_engine.neighbours(features[i])
     print 'queried', dict_feat[i], 'results', zip(*results)[1]
 
+# Create redis storage adapter
+redis_object = Redis(host='localhost', port=6379, db=0)
+redis_storage = RedisStorage(redis_object)
+
+# Get hash config from redis
+config = redis_storage.load_hash_configuration('MyHash')
+
+if config is None:
+    # Config is not existing, create hash from scratch, with 10 projections
+    lshash = RandomBinaryProjections('MyHash', 10)
+else:
+    # Config is existing, create hash with None parameters
+    lshash = RandomBinaryProjections(None, None)
+    # Apply configuration loaded from redis
+    lshash.apply_config(config)
+
+# Create engine for feature space of 100 dimensions and use our hash.
+# This will set the dimension of the lshash only the first time, not when
+# using the configuration loaded from redis. Use redis storage to store
+# buckets.
+engine = Engine(100, lshashes=[lshash], storage=redis_storage)
+
+# Do some stuff like indexing or querying with the engine...
+
+# Finally store hash configuration in redis for later use
+redis_storage.store_hash_configuration(lshash)
 
 
 
+
+
+## RPFOREST TEST
 from rpforest import RPForest
 leaf_size = 5
 n_trees = 20
