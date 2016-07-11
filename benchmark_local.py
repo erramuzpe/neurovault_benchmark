c98248271c4109159eaa7d27c753d755e3d3cc04
erramuzpe <asier.erramuzpe@gmail.com>
2016-07-11 10:44:17 +0200

    modifying benchmark_local

--- benchmark_local.py
+++ benchmark_local.py
@@ -1,14 +1,11 @@
 from django.core.management.base import BaseCommand, CommandError
-from django.test.client import RequestFactory
 from neurovault.apps.statmaps.tests.utils import clearDB
 from neurovault.apps.statmaps.models import User, Collection
-from neurovault.apps.statmaps.views import find_similar
 from neurovault.apps.statmaps.tests.utils import save_statmap_form
 
 import os
 import gc
-import timeit, datetime, tarfile
-import numpy as np
+import timeit
 import pickle
 
 class Timer:
@@ -33,33 +30,15 @@ class Timer:
         if self.verbose:
             print('time taken: %f seconds' % self.interval)
 
-def down_data():
-    import urllib, json
-    if not os.path.isdir('/code/neurovault/apps/statmaps/tests/bench'):
-        os.makedirs('/code/neurovault/apps/statmaps/tests/bench')
-        os.makedirs('/code/neurovault/apps/statmaps/tests/bench/images')
-    if len(os.listdir('/code/neurovault/apps/statmaps/tests/bench/images')) == 0:
-        (path, _) = urllib.urlretrieve("https://ndownloader.figshare.com/files/5360999")
-        archive = tarfile.open(path)
-        archive.extractall('/code/neurovault/apps/statmaps/tests/bench')
-        archive.close()
-        os.remove(path)
-
-
 class Command(BaseCommand):
     args = '<times_to_run>'
     help = 'bench'
 
     def handle(self, *args, **options):
-        down_data()
-
         clearDB()
         app_path = '/code/neurovault/apps/statmaps/tests/bench'
         u1 = User.objects.create(username='neurovault3')
 
-        num_files = len(os.listdir(os.path.join(app_path, 'images/')))
-        index_table = np.zeros(num_files)
-        query_table = np.zeros(num_files)
         dict_feat = {}
 
         for i, file in enumerate(os.listdir(os.path.join(app_path, 'images/'))):
@@ -68,7 +47,6 @@ class Command(BaseCommand):
             randomCollection.save()
 
             t = Timer()
-
             with t:
                 image = save_statmap_form(
                     image_path=os.path.join(app_path, 'images/', file),
@@ -76,7 +54,7 @@ class Command(BaseCommand):
                     image_name=file,
                     ignore_file_warning=True)
             dict_feat[i] = image.pk
-            pickle.dump(dict_feat, open('/code/neurovault/apps/statmaps/tests/dict_feat_localhost.p', "wb"))
             print i
-            if i == 99:
+            if i == 249:
+                pickle.dump(dict_feat, open('/code/neurovault/apps/statmaps/tests/dict_feat_localhost.p', "wb"))
                 break
